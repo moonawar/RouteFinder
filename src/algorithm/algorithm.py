7d51526c7ac1e8982d58b776dfe6653fe719313f
Onyxcodeotto <110540920+Onyxcodeotto@users.noreply.github.com>
2023-04-12 15:06:31 +0700

    delete minimum spannning tree

--- src/algorithm/algorithm.py
+++ src/algorithm/algorithm.py
@@ -1,63 +1,10 @@
 from queue import PriorityQueue
 from algorithm.QELMT import Elmt
-from algorithm.route import Route
-import shlex
-import copy
-import math
-
-def getMinimumSpanningTree(matrix):
-    #Create a copy of matrix and transform it into minimum spanning tree adjecancy matrix
-    MST = [[0 for i in range(len(matrix))] for j in range(len(matrix))]
-    nodeAccessed = [False for i in range(len(matrix))]
-    edge = 0
-    nodeAccessed[0] = True
-    while edge < len(matrix) -1:
-        min = math.inf
-        a = 0
-        b = 0
-        for i in range(len(nodeAccessed)):
-            for j in range(len(nodeAccessed)):
-                if (matrix[i][j] < min) and  (nodeAccessed[i] ^ nodeAccessed[j]) and matrix[i][j] != 0:
-                    min = matrix[i][j]
-                    a = i
-                    b = j 
-        nodeAccessed[a] = True
-        nodeAccessed[b] = True
-        MST[a][b] = min   
-        MST[b][a] = min
-        edge+=1
-    return MST
-
-def expandMST(MST):
-    MST_OR = copy.deepcopy(MST)
-    for i in range(len(MST)):
-        for j in range(len(MST)):
-            if i != j and MST[i][j] == 0:
-                MST_OR[i][j] = DFSMST(MST, i,j, [False for k in range(len(MST))], 0)
-                MST_OR[j][i] = MST_OR[i][j]
-    return MST_OR
-                
-def DFSMST(MST, start, goal,visited, value):
-    visited[start] = True
-    if MST[start][goal] != 0:
-        print(value + MST[start][goal])
-        return value + MST[start][goal]
-    else:
-        for i in range(len(MST)):
-            if i!= start and MST[start][i]!=0 and not visited[i]:
-                temp = DFSMST(MST, i, goal, visited, MST[start][i] + value)
-                if temp!=None:
-                    return temp  
-        return None
-                
+from algorithm.route import Route                
         
 def solve(matrix, start, finish, astar, astarMatrix = None):
     #I.S. start, finish string, exists in graph. Astar boolean.
     #F.S. return path from start to finish
-    if astar:
-        if astarMatrix == None:
-            astarMatrix = getMinimumSpanningTree(matrix)
-            astarMatrix = expandMST(astarMatrix)
             
     queue = PriorityQueue()
     queue.put(Elmt(Route(), start, 0, 0))
